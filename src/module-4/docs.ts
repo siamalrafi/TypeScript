dcdbe65841e75762bb65ff74cb5e05fe2996f28d
siamalrafi <rahathasandipto@gmail.com>
2023-05-09 00:20:05 +0600

    new

--- src/module-4/docs.ts
+++ src/module-4/docs.ts
@@ -1,1 +1,6 @@
-console.log("data is needed");
+// Array and Tuples in typescript
+
+let myArray: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+let myArray1: Array<string> = ["name", "age", "gender"];
+
+myArray.map((a)=>console.log(a))
