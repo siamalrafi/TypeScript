dfd1c1c032bc141baa2a4c88dbabc2ab66560e91
siamalrafi <rahathasandipto@gmail.com>
2023-05-08 16:27:57 +0600

    new code writtend

--- src/module-4/inheritence.ts
+++ src/module-4/inheritence.ts
@@ -1,1 +1,57 @@
-console.log("inheritence");
+class PARENT {
+    name: string;
+    age: number;
+    gender: string;
+
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    getInfo() {
+        console.log(`${this.name}: ${this.age}, ${this.gender}`);
+    }
+}
+
+
+class Student extends PARENT {
+    status: string;
+
+    constructor(name: string, age: number, gender: string, sexy: string) {
+        super(name, age, gender);
+        this.status = sexy;
+    }
+
+    getInfo() {
+        super.getInfo();
+        console.log(`${this.name}: ${this.age}, ${this.gender}, ${this.status}`);
+    }
+    
+}
+
+let student = new Student('John', 20, 'male', 'math');
+
+student.getInfo();
+student.age = 20000;
+student.status = "active"
+student.getInfo();
+
+/* class Teacher extends PARENT {
+    subject: string;
+    constructor(name: string, age: number, gender: string, subject: string) {
+        super(name, age, gender)
+        this.subject = subject
+    }
+    teach(clessOfNumber: number) {
+        console.log(`${this.name} teaches ${this.subject} ${clessOfNumber}`);
+    }
+}
+
+let teacher = new Teacher('John', 20, 'male', 'math');
+teacher.getInfo();
+teacher.age = 4400;
+teacher.getInfo();
+teacher.teach(10);
+
+ */
+
