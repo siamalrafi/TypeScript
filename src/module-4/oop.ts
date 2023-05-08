42cc41ce96264df31ba9afeec7dd40b3284f6e63
siamalrafi <rahathasandipto@gmail.com>
2023-05-08 15:41:34 +0600

    this is the class

--- src/module-4/oop.ts
+++ src/module-4/oop.ts
@@ -1,2 +1,25 @@
-console.log("oop");
-console.log("object");
+class Animal {
+    name: string;
+    age: number;
+    weight: number;
+    sound: string;
+    constructor(name: string, age: number, weight: number, sound: string) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.sound = sound
+    }
+    getAnimal() {
+        console.log(`${this.name} ${this.age} ${this.weight} and ${this.sound}`);
+    }
+}
+const animal = new Animal("Dogy", 4, 23, "Gau Gau");
+animal.getAnimal();
+animal.age = 400;
+animal.weight = 100;
+animal.getAnimal();
+
+
+
+
+
