6238573b17a7d9a7189101a5f7459171df3c3143
siamalrafi <rahathasandipto@gmail.com>
2023-05-08 23:37:28 +0600

    new data is needed

--- src/module-4/abstraction.ts
+++ src/module-4/abstraction.ts
@@ -1,1 +1,33 @@
-console.log("abstraction.ts");
+/* interface IVechile {
+    startEngine: () => void;
+    stopEngine: () => void;
+    move(): void;
+}
+
+class Vechile implements IVechile {
+    constructor(
+        public name: string,
+        public brand: string,
+        public model: number,
+    ) {
+        this.name = name;
+        this.brand = brand;
+        this.model = model;
+    }
+    startEngine() {
+        console.log("Starting engine");
+    }
+    stopEngine() {
+        console.log("Stopping engine");
+    }
+    move() {
+        console.log("Moving");
+    }
+    test(): void {
+        console.log("Test");
+    }
+}
+
+const car = new Vechile("Toyota XX", "Toyota", 44)
+
+console.log(car.name); */
