7e031bf1e1c3f666763d963ea9ba844f3a831f5f
siamalrafi <rahathasandipto@gmail.com>
2023-05-10 21:14:13 +0600

    new

---
+++ src/practice.ts
@@ -0,0 +1,7 @@
+// 1. Convert the following JavaScript array into a TypeScript tuple
+
+// const userInfo = [101, "Ema", "John", true,  , "2023"];
+
+let Nam =  Array<string | number> = ['Apple', 2, 'Orange', 3, 4, 'Banana'];
+
+const Hamal's 
